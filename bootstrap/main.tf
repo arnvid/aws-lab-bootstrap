e4d66736a37ff32f7b3b575a9861630e9925eb77
bogdanfe <bogdan@fetita.ro>
2020-02-12 14:44:51 +0100

    Enabled server side encryption for the S3 bucket
    Enabled server side encryption for the S3 bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -9,7 +9,15 @@ provider "aws" {
 resource "aws_s3_bucket" "tf_state_bucket" {
   bucket = "${var.tf_state_bucket}"
   acl    = "private"
-
+  
+  server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "aws:kms"
+        }
+      }
+    }
+  
   versioning {
     enabled = true
   }
